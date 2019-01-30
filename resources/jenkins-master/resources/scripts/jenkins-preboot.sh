44f283f9e7d69f9eabd75dc67efa2e884295feb1
ottoson-keegan <ottoson_keegan@bah.com>
2019-01-30 13:37:36 -0500

    removing commented-out code

--- resources/jenkins-master/resources/scripts/jenkins-preboot.sh
+++ resources/jenkins-master/resources/scripts/jenkins-preboot.sh
@@ -3,7 +3,5 @@
 # Copyright © 2018 Booz Allen Hamilton. All Rights Reserved.
 # This software package is licensed under the Booz Allen Public License. The license can be found in the License file or at http://boozallen.github.io/licenses/bapl
 
-# replace pipeline-pipeline-multibranch-defaults plugin with our common-workflow plugin
-# rm -rf $JENKINS_HOME/plugins/pipeline-multibranch-defaults* # See below
-# cp /usr/share/jenkins/ref/common-workflow.hpi $JENKINS_HOME/plugins - Removing 1/29/19 (K.O.)
+# Install the JTE Plugin
 cp /usr/share/jenkins/ref/jte.jpi $JENKINS_HOME/plugins
